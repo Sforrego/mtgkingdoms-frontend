85dee4cc2b9ca897af30e957da6b121fe3655724
sforrego <sforrego@uc.cl>
2023-05-18 16:50:55 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import { PublicClientApplication } from '@azure/msal-browser';
+
+const SERVER = 'http://localhost:9998';
+
+const clientId = process.env.REACT_APP_MTGKINGDOMS_CLIENT_ID;
+
+// Initialize the MSAL application object
+const msalConfig = {
+  auth: {
+    clientId: clientId, // Application (client) ID of your registered application
+    authority: 'https://MTGKingdoms.b2clogin.com/MTGKingdoms.onmicrosoft.com/B2C_1_signupsignin', // Azure AD B2C endpoint
+    knownAuthorities: ['MTGKingdoms.b2clogin.com'], // Azure AD B2C domain
+  },
+};
+
+const myMSALObj = new PublicClientApplication(msalConfig);
+
+// Login request
+const loginRequest = {
+  scopes: ['openid', 'profile'],
+};
 
 function App() {
+  const handleLogin = async () => {
+    try {
+      const loginResponse = await myMSALObj.loginPopup(loginRequest);
+      if (loginResponse) {
+        const accountInfo = myMSALObj.getAccountByHomeId(loginResponse.account.homeAccountId);
+        alert(`Hello, ${accountInfo.name}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+};
+
+  const [socket, setSocket] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const newSocket = io(SERVER);
+    setSocket(newSocket);
+
+    newSocket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    newSocket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    newSocket.on('connect_error', (error) => {
+      console.log('Connection error:', error);
+      setTimeout(() => {
+        newSocket.connect();
+      }, 1000); // attempting reconnection every 1 second
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isConnected ? <p>Connected to the server.</p> : <p>Disconnected from the server.</p>}
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
